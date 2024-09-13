e69f97458a9d24eb2b278cb24bc84881bddbd804
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-09-13 17:59:47 -0300

    removido cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
-const cors = require('cors');
 const dotenv = require('dotenv').config();
 
 const app = express();
 
-// Configuração do CORS
-app.use(cors({
-    origin: 'https://buscador-de-veterano-incluir-front.vercel.app', // Permitir apenas o frontend
-    methods: 'GET, POST, PUT, DELETE, OPTIONS', // Métodos permitidos
-    allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
-    credentials: true, // Se for usar cookies ou autenticação
-}));
-
-// Tratamento da solicitação OPTIONS
-app.options('*', cors()); // Responde a todas as requisições OPTIONS
-
-app.use(express.json()); // Certifique-se de que o body das requisições está sendo tratado
+app.use(express.json()); 
 app.use(routes);
 
 app.get('/', (req, res) => {
